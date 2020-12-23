061a0915ba74764fcbfce00a423df523fbbc2a64
faiz9 <sfaiz096@gmail.com>
2020-12-22 23:22:45 -0800

    search added and updated read.me #1

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var IsLoggedIn = require('../middleware/routeprotectors').userIsLoggedIn;
 var getRecentPosts = require('../middleware/postsmiddleware').getRecentPosts;
+var db = require("../conf/database");
 
 /* GET home page. */
 //localhost:3000/login
@@ -24,9 +25,32 @@ router.get('/postimage', (req, res, next) => {
     res.render("postimage", { title: 'Post an Image' });
 });
 
+
 /*
-router.get('/ImagePost', (req, res, next) => {
-    res.render("ImagePost", { title: 'Post' });
+render.get('/post/:id(\\d+)', (req, res, next) => {
+    let baseSQL =
+        " SELECT u.id, u.username, p.title, p.description, p.photopath, p.created \
+FROM users u \
+JOIN posts p \
+ON u.id=fk_userid \
+WHERE p.id=25;";
+
+    let postId = req.params.id;
+    db.execute(baseSQL, [postId])
+        .then(([results, fields]) => {
+            if (results && results.length) {
+                let pos = results[0];
+                res.render('imagepost', { currentPost: post });
+            } else {
+                successPrint('Error! This is not the post you are looking for!')
+                res.redirect('/');
+            }
+        })
+
+});
+
+router.get('/post/help', (req, res, next) => {
+    res.send({ literal: "literal help" });
 }); */
 
 module.exports = router;
